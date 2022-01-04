2964e311598fb496fe4ac38a3ee29a238689ebc9
mutaz1 <89591536+mutaz1@users.noreply.github.com>
2022-01-04 13:24:44 +0300

    Next label fix

--- Scene/Script/DialogBox.gd
+++ Scene/Script/DialogBox.gd
@@ -1,14 +1,14 @@
 extends Control
 
-export var text_speed = 0.05
-export var next_text = 0
+var next_text = 0
+var finished = false
 
 func _ready():
-	$Texts/End.text = " "
+	$Texts/End.text = ""
 
 func _physics_process(delta):
-	if $Texts/Text.visible_characters == len($Texts/Text.text):
-		$Texts/End.text = "Next>"
+	SetName("Actor")
+	WriteLine("Hsfgsdjfjhiosdhflasjdhflaidhfasdhflaisdjhf;kasjdhf;ksfhi", "igdfigodfigjaoijgajgojidfogijapgjapifjgpoaijdfgpofgpojdfogjdoifjgpsofgopsijdfgposjdfgjspgjspdospogosdifjg[oidgsiogjpsogjsgjposifgspogjsoigm spdjfsiodjfposidhfoasdhfiajshdflkjadhfGood", "Bysodfijposifjpaoidjfpoaisdjfoajdfpaijdfeeeeee", "hEllsdifjpoidhfpaiuhsdfiuahsdfiauhsdfiuhadfOlo")
 
 func SetName(Name):
 	$Texts/Name.text = Name
@@ -17,26 +17,28 @@ func AnimationText():
 	while $Texts/Text.visible_characters < len($Texts/Text.text):
 		$Texts/Text.visible_characters += 1
 		
-		yield(get_tree().create_timer(text_speed),"timeout")
+		yield(get_tree().create_timer(1),"timeout")
 
 func WriteLine(text1, text2, text3, text4):
+	if $Texts/Text.visible_characters == len($Texts/Text.text):
+		$Texts/End.text = "Next>"
 	if Input.is_action_just_pressed("ui_accept"):
 		next_text += 1
 		$Texts/Text.visible_characters = 0
-		$Texts/End.text = " "
+		$Texts/End.text = ""
 	match next_text:
-		1:
+		0:
 			AnimationText()
 			$Texts/Text.text = text1
-		2:
+		1:
 			AnimationText()
 			$Texts/Text.text = text2
-		3:
+		2:
 			AnimationText()
 			$Texts/Text.text = text3
-		4:
+		3:
 			AnimationText()
 			$Texts/Text.text = text4
-		5:
+		4:
 			hide()
 			next_text = 0
